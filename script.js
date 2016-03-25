f5c824d6a1be36d3b5746399d2ffb98b6528f246
stefholbrook <stefholbrook@gmail.com>
2016-03-25 22:46:16 +0000

    Add site pages

---
+++ script.js
@@ -0,0 +1,17 @@
+var main = function() {
+     $('.dropdown img').click(function() {
+         $('.dropdown-menu').toggle(); 
+     }); 
+     $('form').submit(function() {
+         var email = $('#email').val(); 
+         var password = $("#password").val(); 
+         if(email === "") {
+             $('.email-error').text("Please enter your email address"); 
+         } 
+         if(password === "") {
+             $('.password-error').text("Please enter a password"); 
+         } 
+         return false; 
+     }); 
+ }; 
+$(document).ready(main); 
